38d39d7b96a065ff6fc295255b4bde42eedb11ed
Anusha Tiwari <anusha20362@iiitd.ac.in>
2021-11-06 00:18:35 +0530

    many operations

--- A3/src/Assgn3/Matrix.java
+++ A3/src/Assgn3/Matrix.java
@@ -1,7 +1,138 @@
 package Assgn3;
 
-public class Matrix {
-    int rows;
-    int columns;
-    int[][] entries;
+public abstract class Matrix {
+    private int rows;
+    private int columns;
+    //int[][] entries;
+
+    Matrix(int r, int c)
+    {
+        this.rows = r;
+        this.columns = c;
+//        entries = new int[this.rows][this.columns];
+//
+//        for (int i=0; i<this.rows; i++)
+//        {
+//            System.arraycopy(arr[i], 0, entries[i], 0, this.columns);
+//        }
+    }
+
+    public abstract int[][] getData();
+    public abstract void add(Matrix other);
+
+    public int getRows()
+    {
+        return this.rows;
+    }
+
+    public int getColumns()
+    {
+        return this.columns;
+    }
+
+    public int[][] division(Matrix other)
+    {
+        if (other instanceof Rectangular)
+        {
+            System.out.println("Cannot divide by rectangular matrix");
+            return null;
+        }
+        if (other instanceof Singular)
+        {
+            System.out.println("Cannot divide by singular matrix");
+            return null;
+        }
+        //int[][] A = this.getData();
+        Matrix B = new Square(other.getRows(), other.getColumns(), other.getData());
+        //int[][] B = other.inverse();
+
+        return this.multiply(B.inverse());
+    }
+
+    public int[][] multiply(Matrix other)
+    {
+        if (this.getRows()!=other.getColumns() || this.getColumns()!=other.getRows())
+        {
+            System.out.println("Multiplication dimensions incorrect");
+            return null;
+        }
+        int[][] product = new int[this.rows][other.getColumns()];
+        int productRows = this.rows;
+        int productColumns = other.getColumns();
+        int answer = 0;
+        int[][] A = this.getData();
+        int[][] B = other.getData();
+
+        for (int i=0; i<productRows; i++)
+        {
+            for (int j=0; j<productColumns; j++)
+            {   answer = 0;
+                for (int k=0; k<this.columns; k++)
+                {
+                    answer+=A[i][k]*B[k][j];
+                }
+                product[i][j] = answer;
+            }
+        }
+        return product;
+    }
+
+    public abstract int[][] elementMul(Matrix other);
+    public abstract int[][] elementDiv(Matrix other);
+
+    public int[][] transpose()
+    {
+        int[][] transpose = new int[this.columns][this.rows];
+        int[][] A = this.getData();
+        for (int i=0; i<this.rows; i++)
+        {
+            for (int j=0; j<this.columns; j++)
+            {
+                transpose[j][i] = A[i][j];
+            }
+        }
+        return transpose;
+    }
+
+    public float rowMean(int row)
+    {
+        int[][] A = this.getData();
+        float answer = 0;
+
+        for (int i=0; i<this.columns; i++)
+        {
+            answer+=A[row][i];
+        }
+        answer/=this.columns;
+        return answer;
+    }
+
+    public float colMean(int col)
+    {
+        int[][] A = this.getData();
+        float answer = 0;
+
+        for (int i=0; i<this.rows; i++)
+        {
+            answer+=A[i][col];
+        }
+        answer/=this.rows;
+        return answer;
+    }
+
+    public float allMean(int col)
+    {
+        int[][] A = this.getData();
+        float answer = 0;
+
+        for (int i=0; i<this.rows; i++)
+        {
+            for (int j=0; j<this.columns; i++)
+            {
+                answer+=A[i][j];
+            }
+        }
+        answer=answer/(this.rows*this.columns);
+        return answer;
+    }
 }
